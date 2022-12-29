03fef088294146f7a136322b74f2b6a6c1de07fd
RyushiAok <55625375+RyushiAok@users.noreply.github.com>
2022-12-29 19:29:00 +0900

    fix

--- client/core/hooks/useUserApi.ts
+++ client/core/hooks/useUserApi.ts
@@ -1,13 +1,7 @@
-import { paths } from "../openapi/openapi"  
-import { Fetcher } from "openapi-typescript-fetch";
-import {MockApiClient} from "../lib/ApiClient"
-
-export default  () => {
-    const test = () => { 
-        const f = MockApiClient() 
-         
-        const getUserFromId = f.path("/users/{id}").method("get").create()
-        const r = getUserFromId({id: 123}) 
+import { UserApi }from '../uitls/mockApi'
+export default () => { 
+    const test = () => {    
+        const r = UserApi.findById({id: 123}) 
         return r.then((r) => { 
             console.log(r.data)
             return r 
---
+++ client/core/uitls/mockApi.ts
@@ -0,0 +1,40 @@
+import {MockApiClient} from "../lib/ApiClient"
+
+const apiClient = MockApiClient()   
+
+export module EStateApi {
+    export const getEStates = apiClient.path("/e-states").method("get").create()
+    export const createEState = apiClient.path("/e-states").method("post").create()
+    export const findById = apiClient.path("/e-states/{id}").method("get").create()
+    export const deleteById = apiClient.path("/e-states/{id}").method("delete").create()
+    export const updateById = apiClient.path("/e-states/{id}").method("patch").create()
+    export const getEventsFilterByEState = apiClient.path("/e-states/{id}/event").method("get").create() 
+}
+export module ETypeApi {
+    export const getETypes= apiClient.path("/e-types").method("get").create()
+    export const createEType = apiClient.path("/e-types").method("post").create()
+    export const findById = apiClient.path("/e-types/{id}").method("get").create()
+    export const deleteById = apiClient.path("/e-types/{id}").method("delete").create()
+    export const updateById = apiClient.path("/e-types/{id}").method("patch").create()
+    export const getEventsFilterByEType = apiClient.path("/e-types/{id}/event").method("get").create() 
+}
+
+export module EventApi {
+    export const getEventList = apiClient.path("/events").method("get").create()
+    export const create = apiClient.path("/events").method("post").create()
+    export const findById = apiClient.path("/events/{id}").method("get").create()
+    export const deleteById = apiClient.path("/events/{id}").method("delete").create()
+    export const updateById = apiClient.path("/events/{id}").method("patch").create()
+    export const getEventStateById = apiClient.path("/events/{id}/state").method("get").create()
+    export const getEventTypeById = apiClient.path("/events/{id}/type").method("get").create()
+    export const getEventMembers = apiClient.path("/events/{id}/users").method("get").create() 
+}
+
+export module UserApi {   
+    export const getUsers = apiClient.path("/users").method("get").create()
+    export const createUser = apiClient.path("/users").method("post").create()
+    export const findById = apiClient.path("/users/{id}").method("get").create()
+    export const deleteById = apiClient.path("/users/{id}").method("delete").create()
+    export const updateById = apiClient.path("/users/{id}").method("patch").create()
+    export const getUsersEvents = apiClient.path("/users/{id}/events").method("get").create() 
+} 

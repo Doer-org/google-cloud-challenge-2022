a508dbb11f0e66ef57c0e3ba5e28e2826ba15475
RyushiAok <55625375+RyushiAok@users.noreply.github.com>
2022-12-30 15:00:51 +0900

    add: hooks(仮)

---
+++ client/core/hooks/useEventCommon.ts
@@ -0,0 +1,59 @@
+ 
+import { fptsHelper } from '../uitls/fptsHelper'
+import {flow, pipe} from 'fp-ts/lib/function'
+import * as TE from 'fp-ts/TaskEither' 
+import { EventApi }from '../uitls/mockApi'  
+import { Event, Host, Participant } from '../types/event'
+
+const tryGetEventInfo = (event_id: number) => { 
+    const getEventInfo = flow (
+        EventApi.findById,
+        fptsHelper.TE.ofApiResponse, 
+    )
+    
+    const getEventHost = flow (
+        EventApi.getEventMembers,
+        fptsHelper.TE.ofApiResponse, 
+    ) 
+    return pipe ( 
+        getEventInfo({id : event_id}), 
+        TE.map((event_info) => pipe (
+            getEventHost({id : event_info.id}),
+            TE.map((members) => {
+                const e : Event =  {
+                    event_id : event_info.id,
+                    event_name : event_info.name,
+                    detail : event_info.detail,
+                    location : event_info.location,
+                    host : {user_id: ""},
+                    participants : members.map((member) => ( 
+                        {
+                            participant_name : member.name,
+                            commemt : ""
+                        }
+                    ))
+                } 
+                return e 
+            })
+        )),
+        TE.flatten
+    )
+}
+
+export default () => {  
+
+    const getEventInfo = (
+        okHandler : (event :  Event) => void,
+        errorHandler : (e: Error) => void,
+    ) => flow (
+        tryGetEventInfo,
+        TE.match(
+            errorHandler,
+            okHandler
+        )
+    )
+
+    return { 
+        getEventInfo, 
+    }
+}

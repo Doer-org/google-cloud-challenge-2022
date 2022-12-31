f1957e2469e9d6b0ca0964674282b898eeab12d6
RyushiAok <55625375+RyushiAok@users.noreply.github.com>
2023-01-01 02:19:10 +0900

    add: スキーマ型定義生成の自動化

---
+++ client/type-gen/build.fs
@@ -0,0 +1,45 @@
+﻿open Fake.Core
+open Fake.IO  
+
+let relativePathFromExe  = 
+    let regex = System.Text.RegularExpressions.Regex(@"\\")  
+    fun absPath -> 
+        System.IO.Path.GetRelativePath(
+            System.Environment.CurrentDirectory, absPath
+        )  
+        |> fun x -> regex.Replace(x, "/")
+
+let initTargets () = 
+    // *** Define Targets ***s
+    Target.create "Default" (fun _ -> 
+        printfn "OpenApi型定義を生成"   
+        let args = 
+            let proto = relativePathFromExe <| Path.combine __SOURCE_DIRECTORY__ @"./../../openapi/openapi.json"  
+            let auto  = relativePathFromExe <| Path.combine __SOURCE_DIRECTORY__ @"./../core/openapi/openapi.ts"  
+            $@"openapi-typescript {proto} " + $@"--output {auto} " 
+        //https://fake.build/reference/fake-javascript-npm.html#install
+        //Npm.run(args) (fun o ->
+        //    { o with
+        //        WorkingDirectory = "."
+        //    }) 
+        // 雑
+        // https://github.com/fsprojects/FAKE/blob/master/src/app/Fake.JavaScript.Npm/Npm.fs#L28-28 
+        ProcessUtils.tryFindFileOnPath "npx"
+        |> function 
+          | Some npx when System.IO.File.Exists npx -> Shell.Exec(npx, args) |> ignore 
+          | _ -> printfn $"npx {args}" 
+        |> ignore 
+    )
+ 
+[<EntryPoint>]
+let main args =
+    args
+    |> Array.toList
+    |> Context.FakeExecutionContext.Create false "build.fsx"
+    |> Context.RuntimeContext.Fake
+    |> Context.setExecutionContext
+
+    initTargets () 
+    // *** Start Build ***
+    Target.runOrDefault "Default"
+    0

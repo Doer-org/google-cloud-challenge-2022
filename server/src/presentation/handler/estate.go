3df78a2e25366ac474d3b75de09182ce0472ebc1
mahiro72 <mahiro.yoshie@gmail.com>
2023-01-07 00:25:32 +0900

    fix: file name

--- server/src/presentation/handler/estate.go
+++
@@ -1,6 +0,0 @@
-package handler
-
-type EStateJson struct {
-	Id   string `json:"id"`
-	Name string `json:"name"`
-}
--- server/src/presentation/handler/etype.go
+++
@@ -1,6 +0,0 @@
-package handler
-
-type ETypeJson struct {
-	Id   string `json:"id"`
-	Name string `json:"name"`
-}

7c1e05ee96a47b486f8501cfd627da949009fe81
RyushiAok <55625375+RyushiAok@users.noreply.github.com>
2023-01-06 13:52:39 +0900

    fix: api mock

--- client/core/api/user/getEventList.ts
+++ client/core/api/user/getEventList.ts
@@ -1,15 +1,48 @@
   
 import {flow} from 'fp-ts/lib/function'
 import * as TE from 'fp-ts/TaskEither' 
+import { Event } from '../../types/event'
  
 
-const tryGetEventList = (user_id : number) => {
-    return TE.right("events")
+export module GetEventListInputsExample {
+    export const causeError = -1
+}
+
+const events : Event[]  =  [ 
+    {
+        event_id: 10,
+        event_name: "a" ,
+        detail: "a" ,
+        location: "a" ,
+        host: {
+            user_id : "1"
+        } ,
+        participants: [] 
+    },
+    {
+        event_id: 20,
+        event_name: "b" ,
+        detail: "b" ,
+        location: "b" ,
+        host: {
+            user_id : "1"
+        } ,
+        participants: [] 
+    }
+]
+ 
+
+
+const tryGetEventList = (user_id : number) : TE.TaskEither<Error, Event[]> => {
+    if(user_id===GetEventListInputsExample.causeError) {
+        return TE.left(Error("tryGetEventList > "))
+    }
+    return TE.right(events)
 }
 
 
 export const getEventList = (
-    okHandler : (events: any) => void,
+    okHandler : (events: Event[]) => void,
     errorHandler : (e: Error) => void,
 ) => flow (
     tryGetEventList,

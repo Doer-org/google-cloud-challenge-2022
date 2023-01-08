91daf0e32d938bb9f17f62a0f55bf814b83d0e73
RyushiAok <55625375+RyushiAok@users.noreply.github.com>
2023-01-09 03:35:58 +0900

    add: delete API

---
+++ client/core/api/event/delete.ts
@@ -0,0 +1,23 @@
+import {flow, pipe} from 'fp-ts/lib/function'
+import * as TE from 'fp-ts/TaskEither'  
+import { fptsHelper } from '../../uitls/fptsHelper'
+import { EventApi }from '../../uitls/mockApi' 
+  
+const tryDeleteEvent = (id : string) => pipe (
+    EventApi.deleteEvent({id: id}),
+    fptsHelper.TE.ofApiResponse 
+)  
+
+export const deleteEvent = (
+    okHandler : (a : any) => void,
+    errorHandler : (e: Error) => void,
+) => flow (
+    tryDeleteEvent,
+    TE.match(
+        errorHandler,
+        okHandler
+    ),
+    (task) => task().then(() => {}),
+    () => {}
+)
+ 

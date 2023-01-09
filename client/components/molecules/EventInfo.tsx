d47866d4ef1e05a9a63979f7466dab014a966a91
nisi0929 <kengo279@icloud.com>
2023-01-09 21:37:02 +0900

    SSR化に伴うLoadScript処理の移行

--- client/components/molecules/EventInfo.tsx
+++ client/components/molecules/EventInfo.tsx
@@ -3,7 +3,6 @@ import { EventWrapper } from './Event/EventWrapper';
 import { Hanging } from './Event/Hanging';
 import { EventBasicInfo } from './Event/EventBasicInfo';
 import { UserIcon } from './User/UserIcon';
-import { LoadScriptTemplate } from '../templates/shared/LoadScriptTemplate';
 export const EventInfo = () => {
   return (
     <>
@@ -15,9 +14,7 @@ export const EventInfo = () => {
           detail="同志社周りのラーメン行こう！！あくたがわとかが良さげ"
         />
         <div className="lg:m-10 m-3">
-          <LoadScriptTemplate>
-            <Map lat={35.6809591} lng={139.7673068} />
-          </LoadScriptTemplate>
+          <Map lat={35.6809591} lng={139.7673068} />
         </div>
       </EventWrapper>
     </>

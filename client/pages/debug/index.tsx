42b1ae02ffa248ddb82f28597f275b79374c6ee0
nisi0929 <kengo279@icloud.com>
2023-01-10 00:15:30 +0900

    wip:微調整

--- client/pages/debug/index.tsx
+++ client/pages/debug/index.tsx
@@ -6,37 +6,36 @@ import {
   CreateParamExample,
 } from '../../core/api/event/create';
 export default function New() {
-  const [createdEventId, setCreatedEventId] = useState(-1);
-  // const createEvent = createNewEvent(
-  //   (v) => {
-  //     console.log(v);
-  //     setCreatedEventId(v.created_event.event_id);
-  //   },
-  //   (v) => {
-  //     setCreatedEventId(-1);
-  //     console.log(v);
-  //   }
-  // );
+  const [createdEventId, setCreatedEventId] = useState('1');
+  const createEvent = createNewEvent(
+    (v) => {
+      console.log(v);
+      setCreatedEventId(v.created_event.event_id);
+    },
+    (v) => {
+      setCreatedEventId('1');
+      console.log(v);
+    }
+  );
 
   return (
     <BasicTemplate className="text-center">
       eventid : {createdEventId}
       <Button
         className="flex m-auto my-5"
-        onClick={
-          () => {}
-          // createEvent({ user_id: '' }, CreateParamExample.causeError)
-        }
+        onClick={() => {
+          createEvent({ user_id: '' }, CreateParamExample.causeError);
+        }}
       >
         fail
       </Button>
       <Button
         className="flex m-auto my-5"
         onClick={() => {
-          // createEvent(
-          //   { user_id: 'abc' },
-          //   { ...CreateParamExample.causeError, max_member: 1 }
-          // );
+          createEvent(
+            { user_id: 'abc' },
+            { ...CreateParamExample.causeError, max_member: 1 }
+          );
         }}
       >
         success

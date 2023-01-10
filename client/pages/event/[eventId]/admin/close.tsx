79dfd6ce497b099fb992120eaa654d24e3b6b5b4
RyushiAok <55625375+RyushiAok@users.noreply.github.com>
2023-01-10 23:36:46 +0900

    TODO対応：admin/close

--- client/pages/event/[eventId]/admin/close.tsx
+++ client/pages/event/[eventId]/admin/close.tsx
@@ -5,21 +5,37 @@ import { BasicTemplate } from '../../../../components/templates/shared/BasicTemp
 import { TypoWrapper } from '../../../../components/atoms/text/TypoWrapper';
 import { Button } from '../../../../components/atoms/text/Button';
 import { EventInfo } from '../../../../components/molecules/EventInfo';
+import { closeEvent } from '../../../../core/api/event/close';
 
 export default function Close() {
   // ここは締め切るページ
   // TODO:event締切hooksをonClickへ
+  const [result, setResult] = useState("not closed")
+  const close = closeEvent(
+    (ok) => {setResult("ok!")},
+    (err) => {setResult("error!")}
+  )
   const [name, setName] = useState('');
   const [word, setWord] = useState('');
-  const event_id = useRouter().query.id;
+  
+  const tmp = useRouter().query.id;
+  const event_id =
+    (typeof(tmp) === "undefined" || Array.isArray(tmp)) ? "" : tmp
+
   return (
     <>
       <MyHead title="イベント締切ページ" description="" />
       <BasicTemplate className="text-center">
         <EventInfo />
-        <Button className="flex m-auto my-5" onClick={() => {}}>
+        <Button 
+          className="flex m-auto my-5" 
+          onClick={() => close(event_id)} 
+        >
           締め切る
         </Button>
+        <>
+          {result}
+        </>
       </BasicTemplate>
     </>
   );

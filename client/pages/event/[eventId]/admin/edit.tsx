2477e73811570e9acd965ae8f9e6b530e51df419
RyushiAok <55625375+RyushiAok@users.noreply.github.com>
2023-01-10 23:36:59 +0900

    TODO対応：admin/edit

--- client/pages/event/[eventId]/admin/edit.tsx
+++ client/pages/event/[eventId]/admin/edit.tsx
@@ -6,9 +6,21 @@ import { MapForm } from '../../../../components/atoms/form/MapForm';
 import { Textarea } from '../../../../components/atoms/form/Textarea';
 import { TypoWrapper } from '../../../../components/atoms/text/TypoWrapper';
 import { BasicTemplate } from '../../../../components/templates/shared/BasicTemplate';
+import { updateEvent } from '../../../../core/api/event/update';
+import { useRouter } from 'next/router';
 
 export default function Edit() {
   // TODO:event編集hooksをonClickへ
+  const [result, setResult] = useState("not closed")
+  const update = updateEvent(
+    (ok) => {setResult("ok!")},
+    (err) => {setResult("error!")}
+  )
+
+  const tmp = useRouter().query.id;
+  const event_id =
+    (typeof(tmp) === "undefined" || Array.isArray(tmp)) ? "" : tmp
+
   const [name, setName] = useState('');
   const [capacity, setCapacity] = useState(1);
   const [detail, setDetail] = useState('');
@@ -40,9 +52,23 @@ export default function Edit() {
           required={true}
         />
         <MapForm />
-        <Button className="flex m-auto my-5" onClick={() => {}}>
+        <Button 
+          className="flex m-auto my-5"
+          onClick={() => 
+            update(
+              { id: event_id,
+                name: name,
+                detail: detail,
+                location:"location",
+                type: "???", 
+                state: "???" } 
+            )
+          }>
           編集完了する
         </Button>
+        <>
+          {result}
+        </>
       </FormWrapper>
     </BasicTemplate>
   );

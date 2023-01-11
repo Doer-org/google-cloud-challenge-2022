a2a7e27f9d15ec4d07d7d1f778a8cdabb7968e56
RyushiAok <55625375+RyushiAok@users.noreply.github.com>
2023-01-11 22:07:36 +0900

    add: interactor test

---
+++ client/__test__/interactor.test.ts
@@ -0,0 +1,18 @@
+import { describe, it, expect, test } from 'vitest' 
+import { flow, pipe } from 'fp-ts/lib/function'
+import * as TE from 'fp-ts/TaskEither'
+import * as E from 'fp-ts/Either'
+import { EventApi } from '../core/utils/gcChallengeApi'  
+import { fptsHelper } from '../core/utils/fptsHelper'
+  
+
+describe('[core/api/event] create', () => {
+  it('正常系：Eventを作成', async () => { 
+    expect(  
+        true
+    )
+    .toBe(
+        true 
+    ) 
+  })
+})

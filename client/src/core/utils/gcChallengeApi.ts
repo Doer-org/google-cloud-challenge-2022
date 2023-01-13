c5cf387fd0ce4a4c0810ea6e3213fc9baee38fc6
RyushiAok <55625375+RyushiAok@users.noreply.github.com>
2023-01-13 15:46:30 +0900

    add todo

--- client/src/core/utils/gcChallengeApi.ts
+++ client/src/core/utils/gcChallengeApi.ts
@@ -2,8 +2,9 @@ import { createApiClient } from '../lib/ApiClient';
 import * as TE from 'fp-ts/TaskEither';
 import { pipe } from 'fp-ts/lib/function';
 
-const baseUrl = process.env.NEXT_PUBLIC_SERVER_URL ?? "http://localhost:8080" 
-    // "https://gc-api-qgai5lo5hq-an.a.run.app"
+const baseUrl = // TODO: テスト用の環境変数を読み込む
+    process.env.NEXT_PUBLIC_SERVER_URL ?? "http://localhost:8080" 
+     //process.env.NEXT_PUBLIC_SERVER_URL ?? "https://gc-api-qgai5lo5hq-an.a.run.app" 
     // // "http://localhost:8080" 
 const apiClient = createApiClient(baseUrl)
 

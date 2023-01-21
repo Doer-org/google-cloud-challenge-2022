b2a9878135bb610c60d295a2f9f8c88819507b01
mahiro72 <mahiro.yoshie@gmail.com>
2023-01-22 03:09:39 +0900

    fix: getMe

--- server/src/infrastructure/google/user.go
+++ server/src/infrastructure/google/user.go
@@ -4,11 +4,10 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
-	"log"
 
 	"github.com/Doer-org/google-cloud-challenge-2022/infrastructure/ent"
 	"github.com/Doer-org/google-cloud-challenge-2022/utils"
+	"github.com/Doer-org/google-cloud-challenge-2022/utils/env"
 )
 
 func (c *Client) GetMe(ctx context.Context) (*ent.User, error) {
@@ -18,33 +17,25 @@ func (c *Client) GetMe(ctx context.Context) (*ent.User, error) {
 	}
 	// tokenを使用して、clientを返す
 	client := c.auth.Config.Client(ctx, token)
-	//TODO:環境変数にする
-	resp, err := client.Get("https://www.googleapis.com/oauth2/v2/userinfo")
+	resp, err := client.Get(env.GetEnvOrDefault("GOOGLE_API_CLIENT",""))
 	if err != nil {
 		return nil, fmt.Errorf("googleapis Get: %w", err)
 	}
 	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return nil, fmt.Errorf("googleapis response ReadAll: %w", err)
+	var gUser googleUser
+	if err:=json.NewDecoder(resp.Body).Decode(&gUser);err!=nil{
+		return nil, fmt.Errorf("decode: %w", err)
 	}
-
-	var ResUser GoogleUser
-	if err := json.Unmarshal(body, &ResUser); err != nil {
-		return nil, fmt.Errorf("googleapis user Unmarshal: %w", err)
-	}
-	//TODO: 消し忘れない
-	log.Println(ResUser)
 	user := &ent.User{
-		Name:          ResUser.Name,
-		Mail:          ResUser.Email,
-		Icon:          ResUser.Picture,
+		Name:          gUser.Name,
+		Mail:          gUser.Email,
+		Icon:          gUser.Picture,
 		Authenticated: true,
 	}
 	return user, nil
 }
 
-type GoogleUser struct {
+type googleUser struct {
 	Id      string `json:"id"`
 	Email   string `json:"email"`
 	Name    string `json:"name"`

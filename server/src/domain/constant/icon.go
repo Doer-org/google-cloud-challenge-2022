f1ecd9b5865c59ce05a33ef336c6add3eea09c18
mahiro72 <mahiro.yoshie@gmail.com>
2023-01-22 00:01:23 +0900

    add: default

--- server/src/domain/constant/icon.go
+++ server/src/domain/constant/icon.go
@@ -1,11 +1,31 @@
 package constant
 
 var (
-	ICON_CAT string = "http://cat.com"
-	ICON_DOG string = "http://dog.com"
+	ICON_CAT string = "http://flat-icon-design.com/f/f_object_111/s512_f_object_111_0bg.png"
+	ICON_DOG string = "http://flat-icon-design.com/f/f_object_112/s512_f_object_112_0bg.png"
+	ICON_SEAL string = "http://flat-icon-design.com/f/f_object_174/s512_f_object_174_0bg.png"
+	ICON_FOX string = "http://flat-icon-design.com/f/f_object_153/s512_f_object_153_0bg.png"
+	ICON_SHEEP string = "http://flat-icon-design.com/f/f_event_98/s512_f_event_98_1bg.png"
+	ICON_CHICKEN string ="http://flat-icon-design.com/f/f_object_161/s512_f_object_161_0bg.png"
+	ICON_BEAR string = "http://flat-icon-design.com/f/f_object_81/s512_f_object_81_2bg.png"
+	ICON_PIG string = "http://flat-icon-design.com/f/f_object_95/s512_f_object_95_1bg.png"
+	ICON_PENGUIN string = "http://flat-icon-design.com/f/f_object_108/s512_f_object_108_2bg.png"
+	ICON_DEER string = "http://flat-icon-design.com/f/f_object_102/s512_f_object_102_0bg.png"
+	ICON_OCTOPUS string = "http://flat-icon-design.com/f/f_object_87/s512_f_object_87_0bg.png"
+	ICON_COW string = "http://flat-icon-design.com/f/f_object_169/s512_f_object_169_0bg.png"
 )
 
 var DEFAULT_ICON_LIST = []string{
 	ICON_CAT,
 	ICON_DOG,
+	ICON_SEAL,
+	ICON_FOX,
+	ICON_SHEEP,
+	ICON_CHICKEN,
+	ICON_BEAR,
+	ICON_PIG,
+	ICON_PENGUIN,
+	ICON_DEER,
+	ICON_OCTOPUS,
+	ICON_COW,
 }

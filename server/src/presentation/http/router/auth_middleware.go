cee6420748cecdc0fe7e211e59896390ed4c48ca
mahiro72 <mahiro.yoshie@gmail.com>
2023-01-21 21:26:47 +0900

    fix: middleware response

--- server/src/presentation/http/router/auth_middleware.go
+++ server/src/presentation/http/router/auth_middleware.go
@@ -24,58 +24,25 @@ func (m *AuthMiddleware) Authenticate(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		sessCookie, err := r.Cookie("session")
 		if err != nil {
-			res.WriteJson(
-				w,
-				res.NewErrJson(
-					http.StatusBadRequest,
-					"StatusBadRequest",
-					fmt.Errorf("falid to get session err : %w", err),
-				),
-				http.StatusBadRequest,
-			)
+			res.WriteJson(w, res.New404ErrJson(fmt.Errorf("error: falid to get session: %w", err)), http.StatusBadRequest)
 			return
 		}
 		userID, err := m.uc.GetUserIDFromSession(sessCookie.Value)
 		if err != nil {
-			res.WriteJson(
-				w,
-				res.NewErrJson(
-					http.StatusBadRequest,
-					"StatusBadRequest",
-					fmt.Errorf("faild to get userId from sessinId: %s , error :  %w", sessCookie.Value, err),
-				),
-				http.StatusBadRequest,
-			)
+			res.WriteJson(w, res.New404ErrJson(fmt.Errorf("error: GetUserIDFromSession: %w", err)), http.StatusBadRequest)
 			return
 		}
 		token, err := m.uc.GetTokenByUserID(userID)
 		if err != nil {
-			res.WriteJson(
-				w,
-				res.NewErrJson(
-					http.StatusBadRequest,
-					"StatusBadRequest",
-					fmt.Errorf("falid to get token err : %w", err),
-				),
-				http.StatusBadRequest,
-			)
+			res.WriteJson(w, res.New404ErrJson(fmt.Errorf("error: GetTokenByUserID: %w", err)), http.StatusBadRequest)
 			return
 		}
-
 		newToken, err := m.uc.RefreshAccessToken(userID, token)
 		if err != nil {
-			res.WriteJson(
-				w,
-				res.NewErrJson(
-					http.StatusBadRequest,
-					"StatusBadRequest",
-					fmt.Errorf("falid to get token err : %w", err),
-				),
-				http.StatusBadRequest,
-			)
+			res.WriteJson(w, res.New404ErrJson(fmt.Errorf("error: RefreshAccessToken: %w", err)), http.StatusBadRequest)
 			return
 		}
-
+		// TODO: なぜ
 		token = newToken
 		r = setToContext(r, userID, token)
 		next.ServeHTTP(w, r)
@@ -90,6 +57,7 @@ func setToContext(r *http.Request, userID string, token *oauth2.Token) *http.Req
 	return r
 }
 
+// TODO: 使われていない
 func setAuthMiddleware(r *chi.Mux, uc *usecase.AuthUsecase) {
 	authmiddle := NewAuthMiddleware(uc)
 	r.Use(authmiddle.Authenticate)

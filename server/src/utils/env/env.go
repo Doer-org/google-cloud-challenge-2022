bf4e0310b2afd6ee58b8cdffdd567e8b07823eed
mahiro72 <mahiro.yoshie@gmail.com>
2023-01-22 02:40:20 +0900

    fix: default

--- server/src/utils/env/env.go
+++ server/src/utils/env/env.go
@@ -1,12 +1,18 @@
 package env
 
-import "os"
+import (
+	"os"
+	"log"
+)
 
 // GetEnvOrDefaultはenvPathに指定された環境変数を取得する
 // 取得できなかった場合はdefaultEnvを利用する
 func GetEnvOrDefault(envPath string, defaultEnv string) string {
 	env := os.Getenv(envPath)
 	if env == "" {
+		if defaultEnv == "" {
+			log.Println("error: env not found and default env is empty too")
+		}
 		return defaultEnv
 	}
 	return env

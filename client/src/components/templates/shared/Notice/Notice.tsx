0c27b4ade9a1bbf1e7881e668881a57dd1a1b063
nisi0929 <kengo279@icloud.com>
2023-01-24 02:59:00 +0900

    notice component仮置き

---
+++ client/src/components/templates/shared/Notice/Notice.tsx
@@ -0,0 +1,22 @@
+import { TypoWrapper } from '../../../atoms/text/TypoWrapper';
+type TProps = {
+  type: 'Error' | 'Success' | 'Normal';
+  text: string;
+};
+// TODO: typeをNormalにしたときは非表示にする
+// displaynoneとかだとanimationが出ないのでなんか考えとく、最悪アニメーションなしで
+// 3秒後とかにNormalにするとかを考えとく（これは上のコンポーネントでtypeがNormalの場合は出さないようにする）
+export const Notice = ({ type, text }: TProps) => {
+  const isError = type === 'Error';
+  return (
+    <div
+      className={`absolute top-5 right-3 py-1 px-3 rounded-md lg:w-1/6 w-1/3 ${
+        isError ? 'bg-red-400' : 'bg-green-400'
+      }`}
+    >
+      <TypoWrapper>
+        <p className="text-center">{text}</p>
+      </TypoWrapper>
+    </div>
+  );
+};
--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -2,11 +2,13 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
+import { Notice } from '../components/templates/shared/Notice/Notice';
 App.getInitialProps = async () => ({ pageProps: {} });
 export default function App({ Component, pageProps }: AppProps) {
   const queryClient = new QueryClient();
   return (
     <QueryClientProvider client={queryClient}>
+      <Notice type={'Error'} text="errorです" />
       <Component {...pageProps} />
     </QueryClientProvider>
   );

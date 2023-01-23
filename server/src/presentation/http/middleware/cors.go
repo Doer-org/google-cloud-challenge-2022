b7156e456c6082ccad9fc31aed228151c5f052e0
mahiro72 <mahiro.yoshie@gmail.com>
2023-01-24 04:03:39 +0900

    fix: false

--- server/src/presentation/http/middleware/cors.go
+++ server/src/presentation/http/middleware/cors.go
@@ -14,14 +14,14 @@ func Cors(next http.Handler) http.Handler {
 			// AllowedOrigins:   []string{"https://foo.com"}, // Use this to allow specific origin hosts
 			// TODO: なおす
 			AllowedOrigins: []string{
-				"https://*",
-				"http://*",
+				"https://gc-shema-app-qgai5lo5hq-an.a.run.app",
+				"http://localhost:3000",
 			},
 			// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
 			AllowedMethods:   []string{http.MethodGet, http.MethodHead, http.MethodPut, http.MethodPatch, http.MethodPost, http.MethodDelete, http.MethodOptions},
 			AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
 			ExposedHeaders:   []string{"Link"},
-			AllowCredentials: true,
+			AllowCredentials: false,
 			MaxAge:           300, // Maximum value not ignored by any of major browsers
 		})
 		next.ServeHTTP(w, r)

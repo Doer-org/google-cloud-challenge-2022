a1604d304f2a87a6395db82139a770ba7d7c3223
mahiro72 <mahiro.yoshie@gmail.com>
2023-01-24 23:04:38 +0900

    mv: cors

--- server/src/presentation/http/middleware/cors.go
+++ server/src/presentation/http/middleware/cors.go
@@ -6,21 +6,13 @@ import (
 	"github.com/go-chi/cors"
 )
 
-func Cors(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// Basic CORS
-		// for more ideas, see: https://developer.github.com/v3/#cross-origin-resource-sharing
-		cors.Handler(cors.Options{
-			// AllowedOrigins:   []string{"https://foo.com"}, // Use this to allow specific origin hosts
-			// TODO: なおす
-			AllowedOrigins: []string{"https://*", "http://*"},
-			// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
-			AllowedMethods:   []string{http.MethodGet, http.MethodHead, http.MethodPut, http.MethodPatch, http.MethodPost, http.MethodDelete, http.MethodOptions},
-			AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
-			ExposedHeaders:   []string{"Link"},
-			AllowCredentials: false,
-			MaxAge:           300, // Maximum value not ignored by any of major browsers
-		})
-		next.ServeHTTP(w, r)
-	})
-}
+var Cors = cors.Handler(cors.Options{
+	// AllowedOrigins:   []string{"https://foo.com"}, // Use this to allow specific origin hosts
+	AllowedOrigins: []string{"https://gc-shema-app-qgai5lo5hq-an.a.run.app", "http://localhost:3000"},
+	// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
+	AllowedMethods:   []string{http.MethodGet, http.MethodHead, http.MethodPut, http.MethodPatch, http.MethodPost, http.MethodDelete, http.MethodOptions},
+	AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+	ExposedHeaders:   []string{"Link"},
+	AllowCredentials: true,
+	MaxAge:           300, // Maximum value not ignored by any of major browsers
+})

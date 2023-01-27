f3224c64ab2f95b800fb4ab99aab4dfd3db82313
mahiro72 <mahiro.yoshie@gmail.com>
2023-01-28 06:17:02 +0900

    fix

--- server/src/usecase/event.go
+++ server/src/usecase/event.go
@@ -139,12 +139,10 @@ func (uc *Event) AddNewEventParticipant(ctx context.Context, eventIdString, name
 	if event.State != constant.STATE_OPEN {
 		return fmt.Errorf("state is not open")
 	}
-	eventUsersCnt, err := uc.repo.GetEventUsersCnt(ctx, eventId)
+	nowSize, err := uc.repo.GetEventUsersCnt(ctx, eventId)
 	if err != nil {
 		return fmt.Errorf("repo.GetEventUsersCnt: %w", err)
 	}
-	// イベント作成者を引く
-	nowSize := eventUsersCnt - 1
 	if event.Size <= nowSize {
 		return fmt.Errorf("the room is already full")
 	}

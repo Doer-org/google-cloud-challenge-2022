4389840ab29238deef01758579b1a7f0637c26c4
RyushiAok <55625375+RyushiAok@users.noreply.github.com>
2023-01-28 06:45:04 +0900

    event_sizeはホストを除く参加可能人数

--- client/src/pages/event/[eventId]/participate/index.tsx
+++ client/src/pages/event/[eventId]/participate/index.tsx
@@ -45,7 +45,8 @@ export default function Participate(event: Event) {
   }, []);
   console.log(event);
   const joinApi = joinEvent(
-    (response: unknown) => {
+    (response) => {
+      console.log(response)
       router.push(`${origin}/event/${event.event_id}/`);
       changeNotice({ type: 'Success', text: '参加しました！' });
     },
@@ -53,7 +54,8 @@ export default function Participate(event: Event) {
   );
   const [name, setName] = useState('');
   const [comment, setComment] = useState('');
-  const isCapacityOver = event.participants.length + 1 > event.event_size;
+  console.log("参加者", event.participants.length, ", event_size", event.event_size)
+  const isCapacityOver = event.participants.length > event.event_size;
   const isTimeOver = new Date(event.close_limit) < new Date();
   const isClosed = event.event_state === 'close';
   return (
